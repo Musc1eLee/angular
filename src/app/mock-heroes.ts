d758bd1b711cd11d24777449d8befed877921b1c
negan <leegenv587@163.com>
2019-01-09 10:48:24 +0800

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+    {id: 2, name: 'batman'},
+    {id: 3, name: 'spiderman'},
+    {id: 4, name: 'Thor'},
+    {id: 5, name: 'ironman'},
+    {id: 6, name: 'superman'},
+    {id: 7, name: 'CaptainAmerica'}
+];
